310ff695c3ee1dca6e4de627a933ed363e1b58b9
LinSrHang <1023948565@qq.com>
2025-10-29 15:38:55 +0800

    feat(常量): 添加HTTP相关常量和网关服务名称
    新增HTTP头部常量定义和网关服务名称，用于统一管理请求头和服务标识

---
+++ constants/http.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	HeaderForwardedByKey  = "X-Forwarded-By"
+	HeaderUserIDKey       = "X-User-ID"
+	HeaderRequestIDKey    = "X-Request-ID"
+	HeaderProxyByKey      = "X-Proxy-By"
+	HeaderLoginTokenKey   = "X-Competition-JWT-Token"
+	HeaderRefreshTokenKey = "X-Competition-Refresh-Token"
+)
+const GatewayServiceName = "OnlineJudge-Controller"
+
+const (
+	ContextUserClaimsKey = "X-Competition-User-Claims"
+)
